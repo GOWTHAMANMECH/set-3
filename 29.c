bef2cbdbf076ff28123ffb69b869c966fbe463ba
GOWTHAMANMECH <35653073+GOWTHAMANMECH@users.noreply.github.com>
2018-03-13 15:55:04 +0530

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a,g;
+    printf("Enter the time in minuits");
+    scanf("%d",&a);
+    g=a/60;
+    printf("\ntime in hours %d",g);
+    printf("\ntime in minute %d",a);
+}
